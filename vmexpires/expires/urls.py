fbe3bec8932d5aee49ce113c82d5639f21abfb31
mboudet <mateo.boudet@gmail.com>
2026-08-27 15:59:02 +0200

    Update urls.py

--- vmexpires/expires/urls.py
+++ vmexpires/expires/urls.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from django.conf.urls import re_path
+from django.urls import re_path
 
 from vmexpires.expires import views
 
